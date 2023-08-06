5f69c4068f6e5d082d9938fc103f6847a9dba8f9
builtbyjosh <joshuacollins912@gmail.com>
2023-08-06 10:21:05 -0400

    setup color palette and install font

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App w-64 h-32 bg-check-background"
+      style={{ backgroundImage: "var(--primary-check-background)" }}
+    >
       <p>TODO APP</p>
     </div>
   );
