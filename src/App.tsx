2b288fd36f1811241e352a151ef7c1f5f2411940
builtbyjosh <joshuacollins912@gmail.com>
2023-08-21 10:21:39 -0500

    build out todo interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-
+import lightDesktopBg from "./images/bg-desktop-light.jpg";
+import darkDesktopBg from "./images/bg-desktop-dark.jpg";
 import "./App.css";
+import TodoHeader from "./components/TodoHeader";
+import { useTodoContext } from "./context/TodoContext";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const { isDarkMode } = useTodoContext();
   return (
-    <div
-      className="App w-64 h-32 bg-check-background"
-      style={{ backgroundImage: "var(--primary-check-background)" }}
-    >
-      <p>TODO APP</p>
+    <div className="relative flex justify-center h-screen">
+      <div className="absolute inset-0 z-0">
+        <img src={isDarkMode ? darkDesktopBg : lightDesktopBg} alt="" />
+      </div>
+      <div className="relative z-10 w-1/2 mt-4 space-y-4">
+        <TodoHeader />
+        <TodoList />
+      </div>
     </div>
   );
 }
