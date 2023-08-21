78fc4206879c180d13b3452afd92c67bf8a2c032
builtbyjosh <joshuacollins912@gmail.com>
2023-08-21 10:11:14 -0500

    expand context logic

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,18 +1,59 @@
-import React, { createContext, useContext, useState } from "react";
-
+import { createContext, useContext, useState } from "react";
+import { v4 as uuid } from "uuid";
 const TodoContext = createContext();
 
 export const useTodoContext = () => useContext(TodoContext);
 
 export const TodoProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  console.log("context todos: ", todos);
+  console.log("filtered: ", filteredTodos);
+  const createNewTodo = (text) => {
+    let newTodo = {
+      id: uuid(),
+      text: text,
+      isChecked: false,
+    };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+
+  const toggleCheck = (todoId) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === todoId ? { ...todo, isChecked: !todo.isChecked } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (todoId) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.todoId !== todoId));
+  };
+
+  const clearCompletedTodos = () => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.isChecked));
+  };
 
   const toggleDarkMode = () => {
     setIsDarkMode((prevMode) => !prevMode);
   };
 
   return (
-    <TodoContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+    <TodoContext.Provider
+      value={{
+        isDarkMode,
+        toggleDarkMode,
+        todos,
+        setTodos,
+        toggleCheck,
+        deleteTodo,
+        clearCompletedTodos,
+        createNewTodo,
+        setFilteredTodos,
+        filteredTodos,
+      }}
+    >
       {children}
     </TodoContext.Provider>
   );
