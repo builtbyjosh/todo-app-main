11c009949680e42d9186eb42b66a301a16b83f40
builtbyjosh <joshuacollins912@gmail.com>
2023-08-21 10:22:26 -0500

    set up header and input

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -2,17 +2,26 @@ import React from "react";
 import moonIcon from "../images/icon-moon.svg";
 import sunIcon from "../images/icon-sun.svg";
 import { useTodoContext } from "../context/TodoContext";
+import TodoInput from "./TodoInput";
 
 const TodoHeader = () => {
   const { isDarkMode, toggleDarkMode } = useTodoContext();
   return (
-    <div>
-      <div>
-        <h1>TODO</h1>
-        <button onClick={toggleDarkMode}>
-          {isDarkMode ? sunIcon : moonIcon}
+    <div className="space-y-2">
+      <div className="flex justify-between items-center">
+        <h1 className="font-extrabold text-white text-2xl">TODO</h1>
+        <button
+          onClick={toggleDarkMode}
+          className=" text-white py-2 px-4 rounded items-center"
+        >
+          <img
+            src={isDarkMode ? sunIcon : moonIcon}
+            alt={isDarkMode ? "Sun Icon" : "Moon Icon"}
+            className="w-5 h-5 border-black"
+          />
         </button>
       </div>
+      <TodoInput />
     </div>
   );
 };
