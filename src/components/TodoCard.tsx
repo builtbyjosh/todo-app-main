fe94ebc14d1a88384e35688685bcf4b386026357
builtbyjosh <joshuacollins912@gmail.com>
2023-08-22 14:45:13 -0500

    add todo logic

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,15 +1,27 @@
 import { useTodoContext } from "../context/TodoContext";
 import TodoCheckBox from "./TodoCheckBox";
+import TodoIconCross from "../images/icon-cross.svg";
 import { TodoType } from "../types";
 
 const TodoCard = (props: { todo: TodoType }) => {
   const { id, text, isChecked } = props.todo;
-  const { toggleCheck } = useTodoContext();
+  const { toggleCheck, deleteTodo, setActiveTodo, activeTodo } =
+    useTodoContext();
 
   return (
-    <div className="flex items-center m-2 py-2">
-      <TodoCheckBox isChecked={isChecked} onChange={() => toggleCheck(id)} />
-      <p className="ml-2">{text}</p>
+    <div
+      onClick={() => setActiveTodo(id)}
+      className="flex items-center justify-between m-2 py-2 text-lightTheme-dark-grayish-blue dark:text-darkTheme-very-dark-grayish-blue"
+    >
+      <div className="flex items-center">
+        <TodoCheckBox isChecked={isChecked} onChange={() => toggleCheck(id)} />
+        <p className="ml-2">{text}</p>
+      </div>
+      {activeTodo === id && (
+        <button onClick={() => deleteTodo(id)}>
+          <img src={TodoIconCross} alt="" />
+        </button>
+      )}
     </div>
   );
 };
