873fb3d3d633f2d2d66e4135029d69363f0572a1
builtbyjosh <joshuacollins912@gmail.com>
2023-08-22 14:35:44 -0500

    add styling

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -22,13 +22,15 @@ const TodoInput: React.FC = () => {
     <form onSubmit={handleInputSubmit}>
       <label className="relative flex items-center">
         <div
-          className={`absolute left-2 top-1/2 transform -translate-y-1/2 w-6 h-6 border rounded-full border-blue-900 flex items-center justify-center`}
-        ></div>
+          className={`absolute left-2 top-1/2 transform -translate-y-1/2 w-6 h-6 border rounded-full bg-gradient-to-r from-blue-300 to-purple-600 flex items-center justify-center`}
+        >
+          <div className="w-5 h-5 dark:bg-darkTheme-very-dark-desaturated-blue bg-lightTheme-very-light-gray rounded-full"></div>
+        </div>
         <input
           type="text"
           value={inputText}
           onChange={handleInputChange}
-          className="pl-12 pr-4 py-2 w-full border rounded focus:outline-none focus:ring focus:border-blue-300"
+          className="pl-12 pr-4 py-2 w-full dark:bg-darkTheme-very-dark-desaturated-blue bg-lightTheme-very-light-gray  rounded focus:outline-none "
           placeholder="Create a new todo..."
         />
       </label>
