763d8e578f658a68a96cfaa49ecbcaf650f5b07e
builtbyjosh <joshuacollins912@gmail.com>
2023-08-22 14:34:58 -0500

    fix gradient

--- src/components/TodoCheckBox.tsx
+++ src/components/TodoCheckBox.tsx
@@ -10,13 +10,25 @@ const TodoCheckBox = ({ isChecked, onChange }: CheckBoxType) => {
   return (
     <label className="flex items-center cursor-pointer">
       <div
-        className={`w-6 h-6 border rounded-full border-gray-300 mr-3 flex items-center justify-center ${
-          isChecked ? "bg-blue-500" : ""
+        className={`w-6 h-6 rounded-full bg-gradient-to-r from-blue-300 to-purple-600 mr-3 flex items-center justify-center ${
+          isChecked ? "bg-gradient-to-r from-blue-300 to-purple-600" : ""
         }`}
       >
-        {isChecked && (
-          <img src={checkBox} alt="Checkmark" className="w-4 h-4 text-white" />
-        )}
+        <div
+          className={`w-5 h-5 rounded-full flex items-center justify-center ${
+            !isChecked
+              ? "dark:bg-darkTheme-very-dark-desaturated-blue bg-lightTheme-very-light-gray"
+              : ""
+          }`}
+        >
+          {isChecked && (
+            <img
+              src={checkBox}
+              alt="Checkmark"
+              className="w-4 h-4 text-white bg-primary-check-background"
+            />
+          )}
+        </div>
       </div>
       <input
         type="checkbox"
