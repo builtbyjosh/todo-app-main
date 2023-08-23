055a34483e4e17f8a9ea739ab6450ecd14e34de2
builtbyjosh <joshuacollins912@gmail.com>
2023-08-23 11:47:21 -0500

    add styling and active button state

--- src/components/TodoListControls.tsx
+++ src/components/TodoListControls.tsx
@@ -31,14 +31,46 @@ const TodoListControls = () => {
   ).length;
 
   return (
-    <div className="text-xs flex items-center justify-between p-2">
+    <div className="text-xs flex items-center justify-between p-2 text-lightTheme-dark-grayish-blue">
       <p>{activeTodosCount} items left</p>
       <div className="space-x-2">
-        <button onClick={() => setFilterBy("all")}>All</button>
-        <button onClick={() => setFilterBy("active")}>Active</button>
-        <button onClick={() => setFilterBy("completed")}>Completed</button>
+        <button
+          onClick={() => setFilterBy("all")}
+          className={
+            filterBy === "all"
+              ? "text-primary-bright-blue"
+              : "hover:text-primary-bright-blue"
+          }
+        >
+          All
+        </button>
+        <button
+          onClick={() => setFilterBy("active")}
+          className={
+            filterBy === "active"
+              ? "text-primary-bright-blue"
+              : "hover:text-primary-bright-blue"
+          }
+        >
+          Active
+        </button>
+        <button
+          onClick={() => setFilterBy("completed")}
+          className={
+            filterBy === "completed"
+              ? "text-primary-bright-blue"
+              : "hover:text-primary-bright-blue"
+          }
+        >
+          Completed
+        </button>
       </div>
-      <button onClick={clearCompletedTodos}>Clear Completed</button>
+      <button
+        onClick={clearCompletedTodos}
+        className="hover:text-primary-bright-blue"
+      >
+        Clear Completed
+      </button>
     </div>
   );
 };
